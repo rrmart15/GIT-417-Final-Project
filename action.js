b97133b7752f1229a52257a5b92defeedbeb2e97
rrmart15 <163369569+rrmart15@users.noreply.github.com>
2024-10-11 22:40:23 -0400

    push

--- action.js
+++ action.js
@@ -1,3 +1,22 @@
-let
+let welcomeSlides = document.getElementById("slideshow");
+
+let j = 0;
+
+function slides() {
+    for (let i = 0; i < slideshow.length; i++) {
+        welcomeSlides[i].style.opacity = 0;
+    }
+
+    slideshow[j].style.opacity = 1;
+
+    if (j < slideshow.length - 1) {
+        j++;
+    } 
+    else {
+        j = 0;
+    }
+}
+
+setInterval(slides, 2000);
 
 
